904a7a89af3989d9b2bd8d402a719223332b77da
sskim <tkfrn4799@gmail.com>
2020-11-13 21:54:50 +0900

    별 찍기-4

---
+++ Algorithms/IO/boj_2441.js
@@ -0,0 +1,21 @@
+let readline = require("readline");
+
+let r = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+r.on("line", (line) => {
+    input.push(line);
+}).on("close", () => {
+    
+    let n = input[0];
+
+    for (let i=0; i < n; i++) {
+        console.log(' '.repeat(i) + "*".repeat(n-i));
+    }
+    
+    process.exit();
+});
