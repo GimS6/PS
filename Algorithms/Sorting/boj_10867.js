57ccbec8f646649a15ab629cac31bcafbb0c93cc
sskim <tkfrn4799@gmail.com>
2020-12-08 21:43:26 +0900

    중복 빼고 정렬, 숫자 카드2

---
+++ Algorithms/Sorting/boj_10867.js
@@ -0,0 +1,23 @@
+const readline = require("readline");
+const r = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let n;
+let l;
+
+r.on("line", (line) => {
+    if (!n) {
+        n = parseInt(line);
+    } else {
+        l = new Set(line.split(" ").map((a) => { return a * 1 }));
+    }
+}).on("close", () => {
+    let result = Array.from(l).
+        sort((a, b) => { return a - b; }).
+        map((a) => { return a + ""; });
+
+    console.log(result.join(" "));
+    process.exit();
+});
